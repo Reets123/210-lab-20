8c634e502cca335f010805ae247532c6aeecc4f5
Reets123 <ibrahimalatig@gmail.com>
2024-10-07 05:57:51 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Lab 20: Chair Maker 3000
+// COMSC-210 - Ibrahim Alatig 
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib> 
+#include <ctime>   
+
+using namespace std;
+
+const int SIZE = 3;
+
+class Chair {
