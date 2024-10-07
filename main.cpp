cf01cd5dacba863dfea5baf6b3f010950a65e535
Reets123 <ibrahimalatig@gmail.com>
2024-10-07 06:26:37 +0000

    commit 1

--- main.cpp
+++ main.cpp
@@ -11,3 +11,19 @@ using namespace std;
 const int SIZE = 3;
 
 class Chair {
+private:
+    int legs;
+    double* prices;
+public:
+    // constructors
+    Chair() {
+        // Seed the random number generator
+        srand(static_cast<unsigned int>(time(0)));
+        
+        // Randomly select 3 or 4 legs
+        legs = (rand() % 2) + 3; // Randomly selects 3 or 4
+        prices = new double[SIZE];
+        
+        // Random prices between $100.00 and $999.99
+        const int MIN = 10000, MAX = 99999;
+        for (int i = 0;
